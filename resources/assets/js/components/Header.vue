c72c904f3cb1e201cefde529f7378305c2154962
arvin <arvin.cao@sunallies.com>
2017-05-10 20:01:03 +0800

    退出登录按钮优化

--- resources/assets/js/components/Header.vue
+++ resources/assets/js/components/Header.vue
@@ -19,51 +19,13 @@
       <!-- Navbar Right Menu -->
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-
-          <!-- User Account Menu -->
-          <li class="dropdown user user-menu">
-            <!-- Menu Toggle Button -->
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <!-- The user image in the navbar-->
-              <img src="http://oe8w1oi0l.bkt.clouddn.com/favicon.ico" class="user-image" alt="User Image">
-              <!-- hidden-xs hides the username on small devices so only the image appears. -->
-              <span class="hidden-xs">Alexander Pierce</span>
+          <li>
+            <a>{{ name }}</a>
+          </li>
+          <li @click='logout'>
+            <a>
+              <el-button size='small' type="text"><span style='color: #fff'>退出</span></el-button>
             </a>
-            <ul class="dropdown-menu">
-              <!-- The user image in the menu -->
-              <li class="user-header">
-                <img src="http://oe8w1oi0l.bkt.clouddn.com/favicon.ico" class="img-circle" alt="User Image">
-
-                <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
-                </p>
-              </li>
-              <!-- Menu Body -->
-              <li class="user-body">
-                <div class="row">
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div>
-                </div>
-                <!-- /.row -->
-              </li>
-              <!-- Menu Footer-->
-              <li class="user-footer">
-                <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
-                </div>
-                <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat" @click="loginOut">Sign out</a>
-                </div>
-              </li>
-            </ul>
           </li>
         </ul>
       </div>
@@ -73,15 +35,27 @@
 
 <script>
   export default{
+    computed: {
+      name () {
+          return sessionStorage.getItem('name')
+      }
+    },
     methods: {
-        loginOut () {
-            sessionStorage.removeItem('token')
-            sessionStorage.removeItem('name')
-            this.$router.push('/login')
+        logout () {
+            this.$confirm('确认退出？', '提示', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+                sessionStorage.removeItem('token')
+                sessionStorage.removeItem('name')
+                this.$router.push('/login')
+            }).catch(() => {
+              return false
+            })
         }
     }
   }
-
 
 </script>
 
